aed6c90a9042c6e189b6f8773004c854e3a75764
m-mohamed <moabdulmohamed@gmail.com>
2024-07-08 12:16:13 -0700

    Configure shadcn data-table

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,15 @@
-import "./App.css"
-import { Counter } from "./features/counter/Counter"
 import { Quotes } from "./features/quotes/Quotes"
-import { Button } from "./components/ui/button"
 import logo from "./logo.svg"
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" className="h-40 w-auto bg-gray-800" />
-        <Button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">
-          Click me
-        </Button>
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Quotes />
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://reselect.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Reselect
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-start min-h-screen p-4 bg-slate-400">
+        <header className="w-full flex justify-center">
+          <img src={logo} alt="logo" className="h-40 w-auto" />
+        </header>
+      </div>
+    </>
   )
 }
 
